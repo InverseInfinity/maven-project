410f44f9106bd137e3071a6ed8423b8c3d6c493c
InverseInfinity <johappyalways@gmail.com>
2022-02-05 11:27:41 +0000

    Jenkins-Docker volume changed to jenkins'workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,5 +5,7 @@ echo ***************************
 echo **** Building app  ********
 echo ***************************
 
+workspace=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
-docker run --rm -v /home/jenkins/jenkins-data/pipeline/java_app:/app -v /root/.m2:/root/.m2 -w /app zenika/alpine-maven:latest "$@"
+
+docker run --rm -v $workspace/java_app:/app -v /root/.m2:/root/.m2 -w /app zenika/alpine-maven:latest "$@"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,8 +4,9 @@ echo ***************************
 echo **** Building Test ********
 echo ***************************
 
+workspace=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 
 
 
-cd /home/jenkins/jenkins-data/pipeline && docker run --rm -v /home/jenkins/jenkins-data/pipeline/java_app/:/app -v /root/.m2/:/root/.m2/ -w /app zenika/alpine-maven:latest "$@"
+cd /home/jenkins/jenkins-data/pipeline && docker run --rm -v $workspace/java_app/:/app -v /root/.m2/:/root/.m2/ -w /app zenika/alpine-maven:latest "$@"
 
