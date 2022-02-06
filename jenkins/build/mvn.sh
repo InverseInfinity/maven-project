05fb30c636666dadbe589785da0dd0c430ee2462
InverseInfinity <johappyalways@gmail.com>
2022-02-06 14:50:07 +0000

    latest update

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,7 +5,8 @@ echo ***************************
 echo **** Building app  ********
 echo ***************************
 
-workspace=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+#workspace=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-build
 
+home=/home/jenkins/jenkins-data/pipeline
 
-docker run --rm -v $workspace/java_app:/app -v /root/.m2:/root/.m2 -w /app zenika/alpine-maven:latest "$@"
+docker run --rm -v $home/java_app:/app -v /root/.m2:/root/.m2 -w /app zenika/alpine-maven:latest "$@"
